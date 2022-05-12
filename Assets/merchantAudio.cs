273cc16d040e4a78d0431b574de04a6e9da95b16
johnmacdmusic <johnmacdmusic@gmail.com>
2022-05-12 14:22:41 -0400

    New SFX in folder + merchant implemented

---
+++ Assets/merchantAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class merchantAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = clips[Random.Range(0, clips.Length)];
+    }
+
+    // Update is called once per frame
+    public void playSound()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = clips[Random.Range(0, clips.Length)];
+            audioSource.Play();
+        }
+    }
+}
